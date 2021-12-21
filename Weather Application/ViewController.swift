4e64b440bde83c3cce36fbe482c38a33a44206f1
RAM111111 <r.r.99@windowslive.com>
2021-12-21 10:50:18 +0300

    Initial Commit

---
+++ Weather Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Application
+//
+//  Created by R on 17/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
